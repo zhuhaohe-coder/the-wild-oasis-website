70497f14927224dabef95ccd17674d6b986ab11b
Zhu Haohe <93075751+zhuhaohe-coder@users.noreply.github.com>
2024-06-19 18:00:25 +0800

    loading

--- app/_components/UpdateProfileForm.tsx
+++ app/_components/UpdateProfileForm.tsx
@@ -2,6 +2,7 @@
 
 import { Guest } from "@/type";
 import { updateProfile } from "@lib/actions";
+import { useFormStatus } from "react-dom";
 
 interface UpdateProfileFormProps {
   guest: Guest;
@@ -59,11 +60,21 @@ function UpdateProfileForm({ guest, children }: UpdateProfileFormProps) {
       </div>
 
       <div className="flex justify-end items-center gap-6">
-        <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-          Update profile
-        </button>
+        <SubmitButton />
       </div>
     </form>
+  );
+}
+
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+      disabled={pending}
+    >
+      {pending ? "Updating..." : "Update profile"}
+    </button>
   );
 }
 
